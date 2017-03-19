8bfb85ebf3b207b377cc3ed5f6e1d84daf5f503b
Nikhil <Nikhil@Nikhil-pc>
2017-03-19 21:08:20 +0530

    Releasing AFZC v4.0 beta 2

--- src/flashablezipcreator/Protocols/Update.java
+++ src/flashablezipcreator/Protocols/Update.java
@@ -27,8 +27,8 @@ public class Update {
     public static String RawUpcomingFeatures = "https://raw.githubusercontent.com/nikhilmenghani/AFZC/v4.0/src/flashablezipcreator/Update/UpcomingFeatures";
     public static float CurrentMainVersion = 4;
     public static float CurrentBetaVersion = 2;
-    public static float CurrentTestVersion = 7;
-    public static String CurrentVersionType = "Test";
+    public static float CurrentTestVersion = 0;
+    public static String CurrentVersionType = "Beta";
     public static boolean isBetaUpdateAvailable = false;
     public static boolean isStableUpdateAvailable = false;
     public static boolean isTestUpdateAvailable = false;
